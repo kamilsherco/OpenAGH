e820025cb3f458fab988d1d43347e37e91eec68a
Kamil <kamilsherco@gmail.com>
2014-05-18 13:58:16 +0200

    test commit

--- src/com/example/openagh/MainActivity.java
+++ src/com/example/openagh/MainActivity.java
@@ -1,15 +1,69 @@
 package com.example.openagh;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
+    private URI url;
+    HttpClient client = new DefaultHttpClient();
+    HttpGet request = new HttpGet(url);
+    HttpResponse response;
+    String html = "cos ";
+    InputStream in;
+    private TextView test;
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test= (TextView)findViewById(R.id.tvURL);
+  
+	/*	try {
+			response = client.execute(request);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+       
+		try {
+			in = response.getEntity().getContent();
+			  BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		        StringBuilder str = new StringBuilder();
+		        String line = null;
+		        while((line = reader.readLine()) != null)
+		        {
+		            str.append(line);
+		        }
+		        in.close();
+		        html = str.toString();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+      test.setText(html);
+        
     }
 
 
