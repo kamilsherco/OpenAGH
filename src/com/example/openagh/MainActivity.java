b5bab96f2dd0cb456a3d200a023bb0071af5c4ba
kamilsherco <kamilsherco@gmail.com>
2014-05-18 15:23:44 +0200

    created connection with server and read html source

--- src/com/example/openagh/MainActivity.java
+++ src/com/example/openagh/MainActivity.java
@@ -1,71 +1,47 @@
 package com.example.openagh;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import java.util.concurrent.ExecutionException;
+
+
 
 import android.os.Bundle;
 import android.app.Activity;
+
 import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    private URI url;
-    HttpClient client = new DefaultHttpClient();
-    HttpGet request = new HttpGet(url);
-    HttpResponse response;
-    String html = "cos ";
-    InputStream in;
-    private TextView test;
+	String html = "cos ";
+
+   
+    private static TextView test;
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        test= (TextView)findViewById(R.id.tvURL);
-  
-	/*	try {
-			response = client.execute(request);
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-       
-		try {
-			in = response.getEntity().getContent();
-			  BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		        StringBuilder str = new StringBuilder();
-		        String line = null;
-		        while((line = reader.readLine()) != null)
-		        {
-		            str.append(line);
-		        }
-		        in.close();
-		        html = str.toString();
-		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+        test = (TextView)findViewById(R.id.tvURL);
+      
+    
+      try {
+		html= new HtmlRead().execute().get();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (ExecutionException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
       test.setText(html);
         
     }
 
+
+	
+	
+	
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
