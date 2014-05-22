3032cce10afda0ebc73ac6e9d209da72f879f5ef
Kamil <kamilsherco@gmail.com>
2014-05-22 18:30:30 +0200

    add vebView

---
+++ src/com/example/openagh/BookActivity.java
@@ -0,0 +1,123 @@
+package com.example.openagh;
+
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class BookActivity extends Activity implements OnItemClickListener {
+	
+	
+   
+    private static ListView test;
+    private ArrayAdapter<String> listAdapter ;
+    
+    private String link;
+    private String classTag;
+    private String linkTag;
+    private String withActivity;
+    
+   
+    
+    private Intent mainIntent;
+    
+    private List<String> respone = new ArrayList<String>();
+    private ArrayList<String> tagList = new ArrayList<String>(); 
+    private List<String> tagLink = new ArrayList<String>();
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_book);
+        test =(ListView)findViewById(R.id.lvURL);
+        
+        mainIntent = getIntent(); // gets the previously created intent
+        link = mainIntent.getStringExtra("link");
+        classTag = mainIntent.getStringExtra("classTag");
+        linkTag = mainIntent.getStringExtra("linkTag");
+        withActivity=mainIntent.getStringExtra("activity");
+        
+        
+        HtmlRead task = new HtmlRead();
+        task.alias=link;
+        task.TagsToClass=classTag;
+        task.TagsToLink=linkTag;
+        
+     	task.execute();
+     	try {
+			respone=task.get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+     	
+     	
+        
+     
+      for (int i = 0; i < respone.size(); i++) {
+    	  if(i%2==0)
+    		tagList.add((respone.get(i)));
+    	  else
+    		  tagLink.add((respone.get(i)));
+  	}
+      
+ 
+      
+      // Create ArrayAdapter using the tag list. 
+      listAdapter = new ArrayAdapter<String>(this,R.layout.simplerow, tagList); 
+      
+      
+      // Set the ArrayAdapter as the ListView's adapter.
+      test.setAdapter( listAdapter );
+      
+      test.setOnItemClickListener(this);
+      
+        
+    }
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		// TODO Auto-generated method stub
+		// Toast.makeText(getApplicationContext(), "pos "+tagLink.get(position), Toast.LENGTH_SHORT).show();
+		if(withActivity.equals("main")){
+		Intent chose = new Intent(this, BookActivity.class);
+     	chose.putExtra("link",tagLink.get(position));
+     	chose.putExtra("classTag","chapter-name");
+     	chose.putExtra("linkTag","div.chapter-name a");
+     	chose.putExtra("activity","book");
+     	startActivity(chose);
+		 
+		}
+		else{
+			Intent chose = new Intent(this, ViewBookActivity.class);
+	     	chose.putExtra("link",tagLink.get(position));
+	     	
+	     	startActivity(chose);
+			 
+			}
+	}
+
+	 @Override
+	    public void onBackPressed() {
+	            super.onBackPressed();
+	            this.finish();
+	            
+	 }
+	
+
+}
---
+++ src/com/example/openagh/ViewBookActivity.java
@@ -0,0 +1,27 @@
+package com.example.openagh;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+@SuppressLint("SetJavaScriptEnabled")
+public class ViewBookActivity extends Activity {
+
+	
+	private Intent mainIntent;
+	private String link;
+
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_viewbook);
+       
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        mainIntent = getIntent(); // gets the previously created intent
+        link = mainIntent.getStringExtra("link");
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadUrl("http://epodreczniki.open.agh.edu.pl/"+link);
+	}
+}
